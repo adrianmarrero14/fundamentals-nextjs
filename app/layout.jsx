de740c8439d88d5cb2c39294fa2fb0b3d4a6323a
Adrian <adrianmarrero18@gmail.com>
2023-09-04 14:16:11 -0400

    metadata created

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,13 @@
+// This is the root layout. It will be used for all pages.
+
 import Navbar from "../components/Navbar"
+
+export const metadata = {
+  title: "Root Layout",
+  description: "This is the root layout",
+  keywords: "root, layout",
+  slug: "/",
+}
 
 export default function RootLayout({ children }) {
   return (
